82c2456ae14fe43ca7470d4459b87690facc6d36
Felix Hummel <felix@felixhummel.de>
2016-10-20 17:43:41 +0200

    wait for reindex to complete

--- elasticsearch_change_mapping/reindex.sh
+++ elasticsearch_change_mapping/reindex.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 base=http://localhost:9200
 
-curl -s -XPOST $base/_reindex?pretty -d @- <<EOF
+# refresh=wait_for blocks until done
+# https://www.elastic.co/guide/en/elasticsearch/reference/master/docs-refresh.html
+curl -s -XPOST "$base/_reindex?pretty&refresh=wait_for" -d @- <<EOF
 {
   "source": { "index": "$1" },
   "dest": { "index": "$2" }
